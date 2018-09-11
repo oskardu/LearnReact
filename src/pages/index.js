2fee1e48b1b15a6b32e9290204398a1b48ed3fc3
杜传虎 <duchuanhu@duchuanhudeMacBook-Pro.local>
2018-09-11 20:11:57 +0800

    animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,8 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <div className="hero">
-      <div className="heroGroup">
-        <h1>Learn to design and code react apps</h1>
+      <div className="heroGroup"> 
+        <h1>Learn to<br/> design and code react apps</h1>
         <p>Complete courses about the best tools and design systems. Prototype and bulid apps with React and Swift. </p>
         <Link to="/page-2/">watch the video</Link>
       </div>
