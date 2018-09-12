c124e02349069d833b33ef2f49f7346089a9d7dd
OskarDu <2292552585@qq.com>
2018-09-12 16:29:14 +0800

    hover animation
    header 里的按钮的悬浮动画效果，观看视频按钮的悬浮动画效果

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ const Header = ({ siteTitle }) => (
   <div className="header">
     <div className="headerGroup">
     <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30"/></Link>
-    <Link to="/course">Courses</Link>
+    <Link to="/courses">Courses</Link>
     <Link to="/downloads">Downloads</Link>
     <Link to="/workshops">Workshops</Link>
     <Link to="/buy"><button>Buy</button></Link>
